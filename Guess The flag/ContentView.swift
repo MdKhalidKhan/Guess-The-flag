7bd219a6d47c607ba6efe4f749ff53abcbe22c01
Raj <rajkhulna@outlook.com>
2022-03-28 02:50:00 +0600

    Initial Commit

---
+++ Guess The flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess The flag
+//
+//  Created by Khalid Khan on 28/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess The flag/Guess_The_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_flagApp.swift
+//  Guess The flag
+//
+//  Created by Khalid Khan on 28/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
